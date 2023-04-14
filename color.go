48679aaa30ab28ac297c787f8e4a7d5833d510ef
alan890104 <alan890104@gmail.com>
2023-04-14 18:30:12 +0800

    color: release for v0.1.0

---
+++ color.go
@@ -0,0 +1,125 @@
+package color
+
+import "fmt"
+
+const (
+	_Black   = "\033[30m"
+	_Red     = "\033[31m"
+	_Green   = "\033[32m"
+	_Yellow  = "\033[33m"
+	_Blue    = "\033[34m"
+	_Magenta = "\033[35m"
+	_Cyan    = "\033[36m"
+	_White   = "\033[37m"
+	_Reset   = "\033[0m"
+)
+
+// Colorize 會將給定的字串加上顏色
+func Colorize(color, text string) string {
+	return color + text + _Reset
+}
+
+// Colorizef 會將給定的字串加上顏色
+func Colorizef(color, format string, a ...interface{}) string {
+	return color + fmt.Sprintf(format, a...) + _Reset
+}
+
+// Blackf 會將給定的字串加上黑色
+func Blackf(format string, a ...interface{}) string {
+	return Colorizef(_Black, format, a...)
+}
+
+// Redf 會將給定的字串加上紅色
+func Redf(format string, a ...interface{}) string {
+	return Colorizef(_Red, format, a...)
+}
+
+// Greenf 會將給定的字串加上綠色
+func Greenf(format string, a ...interface{}) string {
+	return Colorizef(_Green, format, a...)
+}
+
+// Yellowf 會將給定的字串加上黃色
+func Yellowf(format string, a ...interface{}) string {
+	return Colorizef(_Yellow, format, a...)
+}
+
+// Bluef 會將給定的字串加上藍色
+func Bluef(format string, a ...interface{}) string {
+	return Colorizef(_Blue, format, a...)
+}
+
+// Magentaf 會將給定的字串加上紫色
+func Magentaf(format string, a ...interface{}) string {
+	return Colorizef(_Magenta, format, a...)
+}
+
+// Cyanf 會將給定的字串加上青色
+func Cyanf(format string, a ...interface{}) string {
+	return Colorizef(_Cyan, format, a...)
+}
+
+// Whitef 會將給定的字串加上白色
+func Whitef(format string, a ...interface{}) string {
+	return Colorizef(_White, format, a...)
+}
+
+// Black 會將給定的字串加上黑色
+func Black(text string) string {
+	return Colorize(_Black, text)
+}
+
+// Red 會將給定的字串加上紅色
+func Red(text string) string {
+	return Colorize(_Red, text)
+}
+
+// Green 會將給定的字串加上綠色
+func Green(text string) string {
+	return Colorize(_Green, text)
+}
+
+// Yellow 會將給定的字串加上黃色
+func Yellow(text string) string {
+	return Colorize(_Yellow, text)
+}
+
+// Blue 會將給定的字串加上藍色
+func Blue(text string) string {
+	return Colorize(_Blue, text)
+}
+
+// Magenta 會將給定的字串加上紫色
+func Magenta(text string) string {
+	return Colorize(_Magenta, text)
+}
+
+// Cyan 會將給定的字串加上青色
+func Cyan(text string) string {
+	return Colorize(_Cyan, text)
+}
+
+// White 會將給定的字串加上白色
+func White(text string) string {
+	return Colorize(_White, text)
+}
+
+// Success 會將給定的字串加上綠色
+func Success(text string) string {
+	return Colorize(_Green, text)
+}
+
+// Error 會將給定的字串加上紅色
+func Error(text string) string {
+	return Colorize(_Red, text)
+}
+
+// Warning 會將給定的字串加上黃色
+func Warning(text string) string {
+	return Colorize(_Yellow, text)
+}
+
+// Info 會將給定的字串加上藍色
+func Info(text string) string {
+	return Colorize(_Blue, text)
+}
---
+++ color_test.go
@@ -0,0 +1,171 @@
+package color
+
+import "testing"
+
+func TestColorize(t *testing.T) {
+	expected := "\033[31mtest\033[0m"
+	result := Colorize(_Red, "test")
+	if result != expected {
+		t.Errorf("Colorize() failed, expected %q but got %q", expected, result)
+	}
+}
+
+func TestColorizef(t *testing.T) {
+	expected := "\033[32mtest 123\033[0m"
+	result := Colorizef(_Green, "test %d", 123)
+	if result != expected {
+		t.Errorf("Colorizef() failed, expected %q but got %q", expected, result)
+	}
+}
+
+func TestBlackf(t *testing.T) {
+	expected := "\033[30mtest 123\033[0m"
+	result := Blackf("test %d", 123)
+	if result != expected {
+		t.Errorf("Blackf() failed, expected %q but got %q", expected, result)
+	}
+}
+
+func TestRedf(t *testing.T) {
+	expected := "\033[31mtest 123\033[0m"
+	result := Redf("test %d", 123)
+	if result != expected {
+		t.Errorf("Redf() failed, expected %q but got %q", expected, result)
+	}
+}
+
+func TestGreenf(t *testing.T) {
+	expected := "\033[32mtest 123\033[0m"
+	result := Greenf("test %d", 123)
+	if result != expected {
+		t.Errorf("Greenf() failed, expected %q but got %q", expected, result)
+	}
+}
+
+func TestYellowf(t *testing.T) {
+	expected := "\033[33mtest 123\033[0m"
+	result := Yellowf("test %d", 123)
+	if result != expected {
+		t.Errorf("Yellowf() failed, expected %q but got %q", expected, result)
+	}
+}
+
+func TestBluef(t *testing.T) {
+	expected := "\033[34mtest 123\033[0m"
+	result := Bluef("test %d", 123)
+	if result != expected {
+		t.Errorf("Bluef() failed, expected %q but got %q", expected, result)
+	}
+}
+
+func TestMagentaf(t *testing.T) {
+	expected := "\033[35mtest 123\033[0m"
+	result := Magentaf("test %d", 123)
+	if result != expected {
+		t.Errorf("Magentaf() failed, expected %q but got %q", expected, result)
+	}
+}
+
+func TestCyanf(t *testing.T) {
+	expected := "\033[36mtest 123\033[0m"
+	result := Cyanf("test %d", 123)
+	if result != expected {
+		t.Errorf("Cyanf() failed, expected %q but got %q", expected, result)
+	}
+}
+
+func TestWhitef(t *testing.T) {
+	expected := "\033[37mtest 123\033[0m"
+	result := Whitef("test %d", 123)
+	if result != expected {
+		t.Errorf("Whitef() failed, expected %q but got %q", expected, result)
+	}
+}
+
+func TestBlack(t *testing.T) {
+	expected := "\033[30mtest\033[0m"
+	if result := Black("test"); result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestRed(t *testing.T) {
+	expected := "\033[31mtest\033[0m"
+	if result := Red("test"); result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestGreen(t *testing.T) {
+	expected := "\033[32mtest\033[0m"
+	if result := Green("test"); result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestYellow(t *testing.T) {
+	expected := "\033[33mtest\033[0m"
+	if result := Yellow("test"); result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestBlue(t *testing.T) {
+	expected := "\033[34mtest\033[0m"
+	if result := Blue("test"); result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestMagenta(t *testing.T) {
+	expected := "\033[35mtest\033[0m"
+	if result := Magenta("test"); result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestCyan(t *testing.T) {
+	expected := "\033[36mtest\033[0m"
+	if result := Cyan("test"); result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestWhite(t *testing.T) {
+	expected := "\033[37mtest\033[0m"
+	if result := White("test"); result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestSuccess(t *testing.T) {
+	expected := "\033[32msuccess\033[0m"
+	actual := Success("success")
+	if expected != actual {
+		t.Errorf("Expected %s, but got %s", expected, actual)
+	}
+}
+
+func TestError(t *testing.T) {
+	expected := "\033[31merror\033[0m"
+	actual := Error("error")
+	if expected != actual {
+		t.Errorf("Expected %s, but got %s", expected, actual)
+	}
+}
+
+func TestWarning(t *testing.T) {
+	expected := "\033[33mwarning\033[0m"
+	actual := Warning("warning")
+	if expected != actual {
+		t.Errorf("Expected %s, but got %s", expected, actual)
+	}
+}
+
+func TestInfo(t *testing.T) {
+	expected := "\033[34minfo\033[0m"
+	actual := Info("info")
+	if expected != actual {
+		t.Errorf("Expected %s, but got %s", expected, actual)
+	}
+}
